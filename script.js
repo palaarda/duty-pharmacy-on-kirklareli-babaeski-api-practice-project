c9ecc66696e9a32835760030d524016f1f875b02
Arda Pala <50078329+palaarda@users.noreply.github.com>
2023-11-10 04:10:33 +0300

    error-fix-adding-filter

--- script.js
+++ script.js
@@ -1,27 +1,19 @@
 async function getPh(){
-
-    const phData = await fetch('https://www.nosyapi.com/apiv2/pharmacyLink?city=kirklareli&county=babaeski&apikey=cR5vc7B9gtZmOUKCBAQshJcIpPyNRwedlLKM8siczdSqaSYsNeTCTjkne80R', {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer cR5vc7B9gtZmOUKCBAQshJcIpPyNRwedlLKM8siczdSqaSYsNeTCTjkne80R` 
-        }
-    })
-    datajson = await phData.json();
-    aData= datajson.data;
     let pharmacyName = document.getElementById("phName")
     let city = document.getElementById("cityText");
     let adress = document.getElementById("adressText");
     let phoneNumber = document.getElementById("phoneNmb");
+    const pharmacys = await fetch('https://www.nosyapi.com/apiv2/pharmacyLink?city=kirklareli&county=babaeski&apikey=cR5vc7B9gtZmOUKCBAQshJcIpPyNRwedlLKM8siczdSqaSYsNeTCTjkne80R',)
+    datajson = await pharmacys.json();
+    pharmacyData = datajson.data;
     
-    const phS = aData.filter(function(aDatas){
-        return aDatas.Semt == false;
+    const phS = pharmacyData.filter(function(pharmacy){
+        return pharmacy.Semt == false;
     })
-    
-    pharmacyName.innerHTML = phS[0].EczaneAdi;
-    city.innerHTML = phS[0].Sehir;
-    adress.innerHTML = phS[0].Adresi;
-    phoneNumber.innerHTML = phS[0].Telefon;
-
+    const getPharmacy = phS[0];
+    pharmacyName.innerHTML = getPharmacy.EczaneAdi;
+    city.innerHTML = getPharmacy.Sehir;
+    adress.innerHTML = getPharmacy.Adresi;
+    phoneNumber.innerHTML = getPharmacy.Telefon;
 } 
 getPh();
