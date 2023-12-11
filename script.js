03458791292bd2a991c3ab33a1fbe3dfdc5e4000
palaarda <gametime.plz@outlook.com>
2023-12-11 05:40:43 +0300

    update-api-key

--- script.js
+++ script.js
@@ -3,17 +3,20 @@ async function getPh(){
     let city = document.getElementById("cityText");
     let adress = document.getElementById("adressText");
     let phoneNumber = document.getElementById("phoneNmb");
-    const pharmacys = await fetch('https://www.nosyapi.com/apiv2/pharmacyLink?city=kirklareli&county=babaeski&apikey=cR5vc7B9gtZmOUKCBAQshJcIpPyNRwedlLKM8siczdSqaSYsNeTCTjkne80R',)
+    const pharmacys = await fetch('https://api.collectapi.com/health/dutyPharmacy?ilce=babaeski&il=K%C4%B1rklareli', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'apikey 3RUOpnZW6CqXhF34uJchV2:2qzXzLjn2l6WhpMtAOpuqA'
+      }
+    });
     datajson = await pharmacys.json();
     pharmacyData = datajson.data;
-    
-    const phS = pharmacyData.filter(function(pharmacy){
-        return pharmacy.Semt == false;
-    })
-    const getPharmacy = phS[0];
-    pharmacyName.innerHTML = getPharmacy.EczaneAdi;
-    city.innerHTML = getPharmacy.Sehir;
-    adress.innerHTML = getPharmacy.Adresi;
-    phoneNumber.innerHTML = getPharmacy.Telefon;
+
+
+    pharmacyName.innerHTML = datajson.result[1].name;
+    city.innerHTML = datajson.result[1].dist;
+    adress.innerHTML = datajson.result[1].address;
+    phoneNumber.innerHTML = datajson.result[1].phone;
 } 
 getPh();
